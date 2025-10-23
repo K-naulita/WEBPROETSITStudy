877af51cfb7bb40f4aea9375a678b9384771c468
k-naulita <kartikanananaulita@gmail.com>
2025-10-23 08:19:58 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import viteStaticCopy from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
